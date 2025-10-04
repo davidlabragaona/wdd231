a9cbf884f42aa5b8fdbedf06150b9e8ac1dee0dc
David Labra Gaona <davidlabragaona@gmail.com>
2025-10-04 20:34:48 -0300

    w03

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,9 +1,28 @@
+const appID = 'appid';
+
+const geoLobos = {
+    lat: -35.1829,
+    lng: -59.0897
+}
+
+const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${geoLobos.lat}&lon=${geoLobos.lng}&appid=${appID}`;
+
 const icon = document.querySelector('#icon');
 
 async function getWeatherData() {
-    let data = await fetch('www.google.com');
-    data = 'David';
-    return data;
+    try {
+        let response = await fetch(weatherURL);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data.weather[0].main);
+        }
+        else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 
-icon.innerHTML = await getWeatherData();
+icon.innerHTML = getWeatherData();

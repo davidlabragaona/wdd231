62ea4b3190af2060c65b3e498bb1ea9fa6a3b015
David Labra Gaona <davidlabragaona@gmail.com>
2026-01-17 20:36:25 -0300

    partial upload

---
+++ scripts/site.js
@@ -0,0 +1,123 @@
+const courses = [
+    {
+        subject: 'CSE',
+        number: 110,
+        title: 'Introduction to Programming',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.',
+        technology: [
+            'Python'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 130,
+        title: 'Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course introduces students to the World Wide Web and to careers in web site design and development. The course is hands on with students actually participating in simple web designs and programming. It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.',
+        technology: [
+            'HTML',
+            'CSS'
+        ],
+        completed: true
+    },
+    {
+        subject: 'CSE',
+        number: 111,
+        title: 'Programming with Functions',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call , debug, and test their own functions; and to handle errors within functions. CSE 111 students write programs with functions to solve problems in many disciplines, including business, physical science, human performance, and humanities.',
+        technology: [
+            'Python'
+        ],
+        completed: true
+    },
+    {
+        subject: 'CSE',
+        number: 210,
+        title: 'Programming with Classes',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce the notion of classes and objects. It will present encapsulation at a conceptual level. It will also work with inheritance and polymorphism.',
+        technology: [
+            'C#'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 131,
+        title: 'Dynamic Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.',
+        technology: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 231,
+        title: 'Frontend Web Development I',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on user experience, accessibility, compliance, performance optimization, and basic API usage.',
+        technology: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        completed: false
+    }
+];
+
+//================ Dates ===================
+const year = new Date(Date.now());
+document.querySelector('#currentYear').textContent = year.getFullYear();
+document.querySelector('#lastModified').textContent = `Last modified: ${document.lastModified}`;
+
+//hamburguer
+const hamburguer = document.querySelector('#nav-button');
+const menu = document.querySelector('#menu');
+hamburguer.addEventListener('click', x => {
+    hamburguer.classList.toggle('show');
+    menu.classList.toggle('show');
+});
+
+//filter buttons
+document.querySelector('#courseFilters').addEventListener('click', x => {
+    if (x.target.tagName === 'BUTTON') {
+        let coursesFilter = courses;
+        switch (x.target.textContent.toLowerCase()) {
+            case 'cse':
+                coursesFilter = courses.filter(x => x.subject.toLowerCase() === 'cse');
+                break;
+            case 'wdd':
+                coursesFilter = courses.filter(x => x.subject.toLowerCase() === 'wdd');
+                break;
+            default: //all
+                break;
+        }
+        filterCourses(coursesFilter);
+    }
+});
+
+function filterCourses(coursesFilter) {
+    const credits = coursesFilter.filter(x => x.completed === true)
+        .reduce((accumulator, current) => accumulator + current.credits, 0);
+    const res = coursesFilter.map(x => {
+        return `<button class="btn-filter ${x.completed ? 'completed' : 'courses'}">${x.subject}-${x.number}</button>`
+    });
+    document.querySelector('#course-list').innerHTML = res.join('', ',');
+    document.querySelector('#credits').innerHTML = credits;
+    document.querySelector('#courses').textContent = res.length;
+}
+
+filterCourses(courses);
